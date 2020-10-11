1db16ccf109d81ab3732455a287c096efe1d963b
Bridget Shannon <bridgetrshannon@gmail.com>
2020-10-11 18:19:00 -0400

    established database connection

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "rootroot",
+  database: "employee_DB",
+});
---
+++ index.js
@@ -0,0 +1,48 @@
+// Require statements
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const logo = require("asciiart-logo");
+const { start } = require("repl");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "rootroot",
+  database: "employee_DB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // run start function after the connection is made to prompt the user
+  start();
+});
+// Function init()
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
+  console.log(logoText);
+
+  // Function to load prompts aka the questions
+  loadPrompts(
+    inquirer.prompt({
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choice: [
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES",
+        },
+      ],
+    })
+  );
+
+  // Switch statement
+  switch (choice) {
+    case "VIEW_EMPLOYEES":
+      return viewEmployees();
+  }
+}
